eff72265c5de0e792b26479ac6089e8109cdebf7
Joel Travieso <joel@fourkitchens.com>
2019-02-02 01:29:56 -0500

    No Decks text

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -31,8 +31,6 @@ class AddDeck extends Component {
 
     saveDeck({ deck, key })
 
-    // clearLocalNotification()
-    //   .then(setLocalNotification)
   }
 
   render() {
--- components/DeckList.js
+++ components/DeckList.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Platform, TouchableOpacity, FlatList } from 're
 import { connect } from 'react-redux'
 import { receiveDecks } from '../actions'
 import { getDecks } from '../utils/api'
-import { white } from '../utils/colors'
+import { white, globalStyles } from '../utils/colors'
 import DeckItem from './DeckItem'
 import { AppLoading} from 'expo'
 
@@ -30,40 +30,30 @@ class DeckList extends Component {
     }
     
     return (
-      <View>
-        <FlatList
+      <View style={globalStyles.container}>
+      {items.length > 0
+        ? <FlatList
           data={items}
           renderItem={
             (deck) => (
               <DeckItem key={deck.item.key} deckId={deck.item.key} navigation={navigation}/>
             )
           } />
+        : <Text style={styles.noDataText}>No decks available</Text>
+
+
+      }
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: white,
-    borderRadius: Platform.OS === 'ios' ? 16 : 2,
-    padding: 20,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 17,
-    justifyContent: 'center',
-    shadowRadius: 3,
-    shadowOpacity: 0.8,
-    shadowColor: 'rgba(0, 0, 0, 0.24)',
-    shadowOffset: {
-      width: 0,
-      height: 3
-    },
-  },
   noDataText: {
     fontSize: 20,
     paddingTop: 20,
-    paddingBottom: 20
+    paddingBottom: 20,
+    textAlign: 'center'
   }
 })
 
