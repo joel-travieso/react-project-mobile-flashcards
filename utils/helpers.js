9a00779771b4d98d3cacf2c2c4304c80c8f07ea0
Joel Travieso <joel@fourkitchens.com>
2019-02-02 02:17:03 -0500

    Adjust notification time

--- utils/helpers.js
+++ utils/helpers.js
@@ -44,8 +44,8 @@ export function setLocalNotification () {
 
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
-              tomorrow.setHours(0)
-              tomorrow.setMinutes(2)
+              tomorrow.setHours(12)
+              tomorrow.setMinutes(0)
 
               Notifications.scheduleLocalNotificationAsync(
                 createNotification(),
